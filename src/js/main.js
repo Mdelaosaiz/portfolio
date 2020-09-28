642808207ef78a621e024b178a33b7e061938774
Marian de la Osa <mariandelaosa@gmail.com>
2020-09-28 20:19:42 +0200

    added Javascript

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-console.log('>> Ready :)');
+//Variables
+const projects = document.querySelectorAll ('.project');
+
+//Funciones
+
+function openProject() {
+  for (let i of document.querySelectorAll('.open')){
+    i.classList.toggle('open');
+  }
+  this.classList.toggle('open');
+}
+
+function activeProject(e) {
+  console.log(e.propertyName);
+  if (e.propertyName === 'flex-grow'){
+    let tools = e.currentTarget.querySelector('.tools');
+    tools.classList.toggle('open-active');
+  }
+}
+
+//Listeners
+projects.forEach(project => project.addEventListener('click', openProject));
+projects.forEach(project => project.addEventListener('transitionend', activeProject));
+
+
